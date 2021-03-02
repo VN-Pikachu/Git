428afeb0e530ce26fb47a7ca1dc3f41c1275820d
VN-Pikachu <trungchelsea06102001@gmail.com>
2021-03-02 14:28:29 +0700

    Add comment for loading dataset

--- init.py
+++ init.py
@@ -3,5 +3,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+# load iris dataset
 iris = sns.load_dataset('iris')
 sns.scatterplot(data=iris, x='sepal_length', y='sepal_width', hue='species')
